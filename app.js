f4d39c61169e2246a4479c2ddf728354bf7f001b
karla luna <luna_cruzk@hotmail.com>
2026-04-21 15:52:35 -0500

    tarea 10

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const { createApp } = Vue;
 
-createApp({
+// Crear la app
+const app = createApp({
     data() {
         return {
             chuck: [
-                { value: "Chuck Norris can skydive into outer space." },
-                { value: "The chief export of Chuck Norris is pain." },
-                { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
-                { value: "Time waits for no man. Unless that man is Chuck Norris." },
-                { value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
+                {
+                    icon_url: "images/chuck norris.webp",
+                    value: "Chuck Norris can skydive into outer space."
+                },
+                {
+                    icon_url: "images/chuck norris.webp",
+                    value: "The chief export of Chuck Norris is pain."
+                },
+                {
+                    icon_url: "images/chuck norris.webp",
+                    value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants."
+                },
+                {
+                    icon_url: "images/chuck norris.webp",
+                    value: "Time waits for no man. Unless that man is Chuck Norris."
+                },
+                {
+                    icon_url: "images/chuck norris.webp",
+                    value: "If you spell Chuck Norris in Scrabble, you win. Forever."
+                }
             ]
         }
     }
-}).mount('#app');
+});
+
+// Crear el componente
+app.component('chuck-card', {
+    props: ['icon_url', 'value'],
+    template: `
+        <div class="card h-100 shadow-sm">
+            <img :src="icon_url" 
+                class="card-img-top img-fluid p-3"
+                alt="Chuck Norris">
+            <div class="card-body">
+                <p class="card-text text-center">{{ value }}</p>
+            </div>
+        </div>
+    `
+});
+
+// Montar la app
+app.mount('#app');
